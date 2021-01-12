ed80de19674d2273f579230c1aaa9e609f1592f5
Aparajita Dasgupta <adasgupt@buffalo.edu>
2021-01-12 14:46:46 -0500

    adds parametrize with black

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -8,31 +8,44 @@ import molecool
 import pytest
 import sys
 
+
 @pytest.fixture
 def methane_molecule():
-    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    symbols = np.array(["C", "H", "H", "H", "H"])
 
-    coordinates = np.array([
-        [1, 1, 1],
-        [2.4, 1, 1],
-        [-0.4, 1, 1],
-        [1, 1, 2.4],
-        [1, 1, -0.4]
-    ])
+    coordinates = np.array(
+        [[1, 1, 1], [2.4, 1, 1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]]
+    )
 
     return symbols, coordinates
 
-@pytest.mark.parametrize("p1, p2, p3, expected_value", [
-    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45),
-    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
-    (np.array([np.sqrt(3)/2, (1/2), 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 30),
-]
+
+@pytest.mark.parametrize(
+    "p1, p2, p3, expected_value",
+    [
+        (
+            np.array([np.sqrt(2) / 2, np.sqrt(2) / 2, 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            45,
+        ),
+        (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
+        (
+            np.array([np.sqrt(3) / 2, (1 / 2), 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            30,
+        ),
+    ],
 )
 def test_calculate_angle_many(p1, p2, p3, expected_value):
 
     calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
 
-    assert expected_value == pytest.approx(calculated_angle), f'{calculated_angle} {expected_value}'
+    assert expected_value == pytest.approx(
+        calculated_angle
+    ), f"{calculated_angle} {expected_value}"
+
 
 def test_calculate_angle():
 
@@ -43,6 +56,7 @@ def test_calculate_angle():
     expected_value = 90
 
     calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
+
 
 @pytest.mark.skip
 def test_build_bond_list_failure(methane_molecule):
@@ -58,10 +72,11 @@ def test_build_bond_list(methane_molecule):
     _, coordinates = methane_molecule
     bonds = molecool.build_bond_list(coordinates)
 
-    assert len(bonds) ==4
+    assert len(bonds) == 4
 
     for bond_length in bonds.values():
         assert bond_length == 1.4
+
 
 def test_calculate_distance():
     """Test that the calculate distance function calculates what we expect."""
@@ -74,6 +89,7 @@ def test_calculate_distance():
     observed_distance = molecool.calculate_distance(r1, r2)
 
     assert expected_distance == observed_distance
+
 
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
